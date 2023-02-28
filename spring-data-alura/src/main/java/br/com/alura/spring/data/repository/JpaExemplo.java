06c7ced67e0fe8ed408f651dd4700161904e83e5
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-02-28 08:32:45 -0300

    aula 01 - SpringDataJPA

---
+++ spring-data-alura/src/main/java/br/com/alura/spring/data/repository/JpaExemplo.java
@@ -0,0 +1,42 @@
+package br.com.alura.spring.data.repository;
+
+import br.com.alura.spring.data.orm.Cargo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+//Exemplo de classe utilizando somente o JPA para fins didáticos
+public class JpaExemplo {
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public JpaExemplo() {
+        emf = Persistence.createEntityManagerFactory("jpa");
+        em = emf.createEntityManager();
+    }
+
+    public void save(Cargo cargo){
+        em.getTransaction().begin();
+        em.persist(cargo);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Cargo findById(Integer id){
+        em.getTransaction().begin();
+        Cargo cargo = em.find(Cargo.class, id);
+        em.getTransaction().commit();
+        em.close();
+        return cargo;
+    }
+
+    public void deleteById(Integer id){
+        Cargo cargo = em.find(Cargo.class, id);
+        em.getTransaction().begin();
+        em.remove(cargo);
+        em.getTransaction().commit();
+        //em.close();
+    }
+
+
+}
